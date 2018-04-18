d9d51d807f9b15e9bfc515c0005fe268d763b6f8
ciel <ciel@qq.com>
2018-04-18 17:03:31 +0800

    django Page49

--- sign/views.py
+++ sign/views.py
@@ -11,12 +11,14 @@ def login_action(request):
         password = request.POST.get("password", "")
         if username == "zyf" and password == "123":
             response = HttpResponseRedirect("/event_manage/")
-            response.set_cookie("user", username, 3600)#添加浏览器cookie
+            #response.set_cookie("user", username, 3600)#添加浏览器cookie
+            request.session['user_session'] = username#将session信息记录到浏览器
             return response
         else:
             return render(request, "index.html", {"error": "username or password error!"})
 
 #发布会管理
 def event_manage(request):
-    username = request.COOKIES.get("user", "")
+    #username = request.COOKIES.get("user", "")
+    username = request.session.get("user_session", "")#读取浏览器session
     return render(request, "event_manage.html", {"user":username})
